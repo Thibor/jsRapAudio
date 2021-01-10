cf6c02fbe19106858b1833fcda8d3cb1cf7190dc
Thibor <thibor@vp.pl>
2021-01-10 03:30:35 +0000

    update

--- jsRapAudio.js
+++ jsRapAudio.js
@@ -137,9 +137,9 @@ window.AudioContext = window.AudioContext || window.webkitAudioContext || window
 			function RenderFrame() {
 				analyser.getByteFrequencyData(frequencyData);
 				ctx.clearRect(0, 0, cwidth, cheight);
-				for (var i = 0; i < meterNum; i++) {
-					var x = meterStart + i * meterSpace;
-					var y = cheight - (cheight * frequencyData[i * step]) / 0xff;
+				for (let i = 0; i < meterNum; i++) {
+					let x = meterStart + i * meterSpace;
+					let y = cheight - (cheight * frequencyData[i * step]) / 0xff;
 					if (y > capYPositionArray[i])
 						capYPositionArray[i] += capSpeed;
 					else
@@ -155,7 +155,7 @@ window.AudioContext = window.AudioContext || window.webkitAudioContext || window
 			}
 
 			function SecToTim(secs) {
-				var hoursDiv = secs / 3600, hours = Math.floor(hoursDiv), minutesDiv = secs % 3600 / 60, minutes = Math.floor(minutesDiv), seconds = Math.ceil(secs % 3600 % 60);
+				let hoursDiv = secs / 3600, hours = Math.floor(hoursDiv), minutesDiv = secs % 3600 / 60, minutes = Math.floor(minutesDiv), seconds = Math.ceil(secs % 3600 % 60);
 				if (seconds > 59) { seconds = 0; minutes = Math.ceil(minutesDiv); }
 				if (minutes > 59) { minutes = 0; hours = Math.ceil(hoursDiv); }
 				return (hours == 0 ? '' : hours > 0 && hours.toString().length < 2 ? '0' + hours + ':' : hours + ':') + (minutes.toString().length < 2 ? '0' + minutes : minutes) + ':' + (seconds.toString().length < 2 ? '0' + seconds : seconds);
